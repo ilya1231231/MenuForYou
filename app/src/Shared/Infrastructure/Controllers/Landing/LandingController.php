6908aa5b1d95e82f2ef0e8c52183e3ae6228f061
ilya1231231 <malygin.ilja@yandex.ru>
2024-03-11 20:54:56 +0300

    Пытаюст собрать лендинг

---
+++ app/src/Shared/Infrastructure/Controllers/Landing/LandingController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Shared\Infrastructure\Controllers\Landing;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class LandingController extends AbstractController
+{
+    #[Route('/')]
+    public function getMainPage(Request $request): Response
+    {
+        return new Response(
+            ''
+        );
+    }
+}
