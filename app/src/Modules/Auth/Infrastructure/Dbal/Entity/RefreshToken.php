b1fb0eb2dd9ddf5333c6af9f444c850540e24fb5
ilya1231231 <malygin.ilja@yandex.ru>
2024-07-31 19:48:12 +0300

    Добавил реализацию рефреш токена

---
+++ app/src/Modules/Auth/Infrastructure/Dbal/Entity/RefreshToken.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Modules\Auth\Infrastructure\Dbal\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gesdinet\JWTRefreshTokenBundle\Entity\RefreshToken as BaseRefreshToken;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'refresh_tokens')]
+class RefreshToken extends BaseRefreshToken
+{
+}
