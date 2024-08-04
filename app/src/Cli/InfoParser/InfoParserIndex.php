f3e18db6caae2d8b3bccf1667a2bded13b3a083a
ilya1231231 <malygin.ilja@yandex.ru>
2024-08-04 20:55:03 +0300

    Сделал сохраниение спаршенной погоды

--- app/src/Cli/InfoParser/InfoParserIndex.php
+++ app/src/Cli/InfoParser/InfoParserIndex.php
@@ -4,6 +4,7 @@ namespace App\Cli\InfoParser;
 
 use App\Modules\MailRuWeather\Infrastructure\Dbal\Entity\MailRuWeather;
 use App\Modules\MailRuWeather\Infrastructure\Dbal\Repository\MailRuWeatherRepository;
+use DateTime;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -23,7 +24,6 @@ class InfoParserIndex extends Command
     //php bin/console parse:start_parse_info
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->testSave();
         //find forecasts
         $rawHtml = file_get_contents('https://pogoda.mail.ru/prognoz/yoshkar-ola/24hours/');
 
@@ -65,29 +65,33 @@ class InfoParserIndex extends Command
 
         foreach ($todayForecast as $forecast) {
             if (!isset($forecast['time'], $forecast['tempe'], $forecast['tempe_comf'], $forecast['precip_prob'])) {
-                //залогированить
+                //залогировать
                 continue;
             }
 
-            $time = 'Время ' . $forecast['time'];
-            $temp = 'Температура '. $forecast['tempe'];
-            $tempSense = 'Ощущается как '. $forecast['tempe_comf'];
-            $rainChance = 'Вероятность осадков '. $forecast['precip_prob'] . '%';
-            print_r(implode('; ', [$time, $temp, $tempSense, $rainChance]). PHP_EOL);
+            $mailRuWeather = new MailRuWeather();
+
+            $explodedTime = explode(':', $forecast['time']);
+            $hour = $explodedTime[0];
+            $minutes = $explodedTime[1];
+            $datetime = new DateTime();
+            $datetime = $datetime->setTime($hour, $minutes);
+            $mailRuWeather->setDatetime($datetime);
+
+            $mailRuWeather->setTemp($forecast['tempe']);
+            $mailRuWeather->setTempSense($forecast['tempe_comf']);
+            $mailRuWeather->setRainChance($forecast['precip_prob']);
+            $this->mailRuWeatherRepository->save($mailRuWeather);
+
+//            $time = 'Время ' . $forecast['time'];
+//            $temp = 'Температура '. $forecast['tempe'];
+//            $tempSense = 'Ощущается как '. $forecast['tempe_comf'];
+//            $rainChance = 'Вероятность осадков '. $forecast['precip_prob'] . '%';
+//            print_r(implode('; ', [$time, $temp, $tempSense, $rainChance]). PHP_EOL);
         }
 
 
 
         return 1;
-    }
-
-    private function testSave()
-    {
-        $mailRuWeather = new MailRuWeather();
-        $mailRuWeather->setDatetime(new \DateTime());
-        $mailRuWeather->setRainChance(2);
-        $mailRuWeather->setTempSense('+20');
-        $mailRuWeather->setTemp('+29');
-        $this->mailRuWeatherRepository->save($mailRuWeather);
     }
 }
